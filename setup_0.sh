63261095d0b7052e8d53cefc876f46a49c301f18
Greg Wiley <gwiley@cj.com>
2016-01-15 11:25:24 -0800

    add user bin dirs

--- setup_0.sh
+++ setup_0.sh
@@ -3,3 +3,10 @@
 am-mac() {
   test "$OSTYPE" = "darwin15"
 }
+
+# well-known user binary locations
+for dir in ${HOME}/{bin,.local/bin}; do
+  if [ -d $dir ]; then
+    export PATH=${PATH}:${dir}
+  fi
+done
