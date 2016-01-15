b5c6a710f26425898624847a94f686dc3c26124d
Greg Wiley <gwiley@cj.com>
2016-01-15 12:08:12 -0800

    wrap setups in functions

--- zero.sh
+++ zero.sh
@@ -7,3 +7,31 @@ am-mac() {
 am-linux() {
   ! $(am-mac)
 }
+
+_bash-util-dir() {
+  dirname ${BASH_SOURCE[0]}
+}
+
+bash-util-interactive() {
+  local recopt=$(shopt -p nullglob)
+  shopt -s nullglob
+  local files=($(_bash-util-dir)/interactive_* )
+  if [ ${#files[@]} -gt 0 ]; then
+    for s in $(_bash-util-dir)/interactive_*; do
+      source $s
+    done
+  fi
+  eval $recopt
+}
+
+bash-util-common() {
+  local recopt=$(shopt -p nullglob)
+  shopt -s nullglob
+  local files=($(_bash-util-dir)/common_*)
+  if [ ${#files[@]} -gt 0 ]; then
+    for s in $(_bash-util-dir)/common_*; do
+      source $s
+    done
+  fi
+  eval $recopt
+}
