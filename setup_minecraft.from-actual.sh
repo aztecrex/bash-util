d84a5ed0b4774ea6ccf4e4bf79955062cb87422c
Greg Wiley <gwiley@cj.com>
2016-01-15 11:36:15 -0800

    merge minecraft

--- setup_minecraft.from-actual.sh
+++
@@ -1,2 +0,0 @@
-alias minecraft='XMODIFIERS= java -jar $HOME/.lib/Minecraft.jar > /dev/null < /dev/null 2>&1 & disown'
-
--- setup_minecraft.sh
+++ setup_minecraft.sh
@@ -1,2 +1,9 @@
-# prevent keyboard lockups in minecraft (method works for any java-based ui)
-alias minecraft='XMODIFIERS= java -jar $HOME/.lib/Minecraft.jar > /dev/null < /dev/null 2>&1 & disown'
+
+if [ -f $HOME/.lib/Minecraft.jar ]; then
+  if $(am-linux); then
+    # LINUX: prevent keyboard lockups in minecraft (method works for any java-based ui)
+    alias minecraft='XMODIFIERS= java -jar $HOME/.lib/Minecraft.jar > /dev/null < /dev/null 2>&1 & disown'
+  else
+    alias minecraft='java -jar $HOME/.lib/Minecraft.jar > /dev/null < /dev/null 2>&1 & disown'
+  fi
+fi
